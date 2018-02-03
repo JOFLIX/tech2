1e59d80d4be6ede970aafacd299bc61fa5b6b52c
Ansar Zhalyalov <ansaril3g@gmail.com>
2018-02-03 18:03:45 +0300

    mail text
    text

--- post.php
+++ post.php
@@ -6,7 +6,7 @@ $usertel = $_POST['message']; // сохраняем в переменную да
 $usermail = $_POST['email']; // сохраняем в переменную данные полученные из поля c адресом электронной почты
  
 // Формирование заголовка письма
-$subject  = "Новое сообщение";
+$subject  = "Новое сообщение - Телемедицина";
 $headers  = "From: " . strip_tags($usermail) . "\r\n";
 $headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
@@ -14,7 +14,7 @@ $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
  
 // Формирование тела письма
 $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
-$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Cообщение с сайта</h2>\r\n";
+$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Cообщение с medicine-live.ru/tech</h2>\r\n";
 $msg .= "<p><strong>От кого:</strong> ".$username."</p>\r\n";
 $msg .= "<p><strong>Почта:</strong> ".$usermail."</p>\r\n";
 $msg .= "<p><strong>Текст:</strong> ".$usertel."</p>\r\n";
@@ -22,9 +22,9 @@ $msg .= "</body></html>";
  
 // отправка сообщения
 if(@mail($sendto, $subject, $msg, $headers)) {
-    echo "Ваш запрос отправлен!";
+    echo "<html><head><meta http-equiv="refresh" content="5;http://medicine-live.ru/tech"></head><body>Ваш запрос упешно отправлен!<br> Через 4 секунды вы будете возвращены на страницу</body></html>";
 } else {
-    echo "Проблема с отправкой запроса, напишите на почту medicineliveru@gmail.com";
+    echo "<html><head><meta http-equiv="refresh" content="5;http://medicine-live.ru/tech"></head><body>Проблема с отправкой запроса, напишите нам на почту medicineliveru@gmail.com!<br> Через 4 секунды вы будете возвращены на страницу</body></html>";
 }
  
 ?>
